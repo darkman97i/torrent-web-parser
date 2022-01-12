930d0cb3ca6a01f3bea337cd8b12ccd9f2866ca3
Josep Llort <jllort@openkm.com>
2022-01-12 14:40:59 +0100

    Use of parameter u cause wrong geckoDriverPath initialization
    fix #13

--- src/main/java/info/llort/torrent/Config.java
+++ src/main/java/info/llort/torrent/Config.java
@@ -1,7 +1,7 @@
 package info.llort.torrent;
 
 public abstract class Config {
-	public static final String URL_WEB_TO_PARSE = "https://atomixhq.net";
+	public static final String URL_WEB_TO_PARSE = "https://atomixhq.top";
 	public static final String FIREFOX_DRIVER_PATH = "/home/jllort/git/torrentwebparser/geckodriver";
 	public static final String FILESYSTEM_DOWNLOAD_PATH = "/home/jllort/Descargas";
 	public static long FILE_DOWNLOAD_TIMEOUT = 2;
